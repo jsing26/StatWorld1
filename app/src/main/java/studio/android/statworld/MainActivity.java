796355d64e4e0390de6b9d9ceba09ca1c5f6c296
604466 <jtsing2002@gmail.com>
2018-03-01 10:46:09 -0600

    update 1.0

--- app/src/main/java/studio/android/statworld/MainActivity.java
+++ app/src/main/java/studio/android/statworld/MainActivity.java
@@ -9,36 +9,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Spinner myspinner = (Spinner) findViewById(R.id.spinner);
+        Spinner2 myspinner2 = (Spinner) findViewById(R.id.spinner2);
+        Spinner3 myspinner3 = (Spinner) findViewById(R.id.spinner3);
     }
 
 }
