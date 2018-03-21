d476b72b3e2f627b5af7ff6c49ca972879163177
604466 <jtsing2002@gmail.com>
2018-03-21 10:01:34 -0500

    update 1.0

--- app/src/main/java/studio/android/statworld/MainActivity.java
+++ app/src/main/java/studio/android/statworld/MainActivity.java
@@ -2,12 +2,17 @@ package studio.android.statworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     Spinner spinner2;
     Spinner spinner3;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2  = (Spinner) findViewById(R.id.spinner2);
         spinner3  = (Spinner) findViewById(R.id.spinner3);
+
+        adapter = ArrayAdapter.createFromResource(this,R.array.state_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+"selected",Toast.LENGTH_LONG) .show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+            }
 }
 
-}
+
